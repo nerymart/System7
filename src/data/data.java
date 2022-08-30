ed8c05e3b3b7471ab5209db1b54145e52f0f1d2c
nerymart <martineznery365@gmail.com>
2022-08-29 18:21:40 -0600

    Nuevos datos

---
+++ src/data/data.java
@@ -0,0 +1,36 @@
+package data;
+
+public class data {
+	private int  velocidad;
+	private int tiempo;
+	private int distancia;
+	public data(int velocidad, int tiempo, int distancia) {
+		super();
+		this.velocidad = velocidad;
+		this.tiempo = tiempo;
+		this.distancia = distancia;
+	}
+	public int getVelocidad() {
+		return velocidad;
+	}
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+	public int getTiempo() {
+		return tiempo;
+	}
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+	public int getDistancia() {
+		return distancia;
+	}
+	public void setDistancia(int distancia) {
+		this.distancia = distancia;
+	}
+	
+
+	
+	
+
+}
--- src/main/MRU.java
+++ src/main/MRU.java
@@ -1,6 +1,57 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import data.data;
+
 public class MRU {
+	ArrayList<data> mr = new ArrayList<data>();
+	Scanner sc = new Scanner (System.in);
+public void menu(){
+	
+	boolean salir = false ;
+	int op;
+	while (!salir) {
+		System.out.print("1 : Calcular el tiempo");
+		System.out.print("2 : Calcular velocidad");
+		System.out.print("3 : calcular tiempo");
+		System.out.print("4 : Salir");
+		op= sc.nextInt();
+		
+		switch(op) {
+		case 1:
+			break;
+		case 2:
+			break;
+	
+		case 3:
+			break;
+		case 4:
+			break;
+			
+		}
+		
+		
+	}
+}
+
+	
+	public void tiempo(){
+		System.out.println("Dijite la distancia");
+		
+		System.out.println("Dijite la velocidad");
+		System.out.println("");
+		
+		
+		
+	}
+	public void  velocidad(){
+		
+	}
+	public void  distancia (){
+		
+	}
 	
 
 }
